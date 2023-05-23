63b7ac53fc88456860f45443ed0745d496fafbf7
George Barrett <georgeaustinbarrett@gmail.com>
2023-05-23 13:54:48 +0100

    amendment to test structure

--- week1_challenges/testing_with_jest/add.test.js
+++ week1_challenges/testing_with_jest/add.test.js
@@ -2,6 +2,6 @@ const add = require('./add');
 
 describe('add', () => {
     it('adds 2 and 2', () => {
-        expect(2 + 2).toBe(4);
+        expect(add(2, 2)).toBe(4);
       });
 });
--- week1_challenges/testing_with_jest/multiply.test.js
+++ week1_challenges/testing_with_jest/multiply.test.js
@@ -2,6 +2,6 @@ const multiply = require('./multiply');
 
 describe('multply', () => {
     it('multiplies 3 and 3', () => {
-        expect(3 * 3).toBe(9);
+        expect(multiply(3, 3)).toBe(9);
       });
 });
