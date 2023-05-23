c6c919d9ff29afe23cec0ddfd8c95fec8ef6a1c1
George Barrett <georgeaustinbarrett@gmail.com>
2023-05-23 17:15:21 +0100

    more tests to cover edge cases

--- week1_challenges/testing_with_jest/searchCandies.test.js
+++ week1_challenges/testing_with_jest/searchCandies.test.js
@@ -16,4 +16,16 @@ describe("searchCandies", () => {
   it("Ma, 10", () => {
     expect(searchCandies("Ma", 10)).toEqual(["Mars", "Maltesers"]);
   });
+  it("Ma, 2", () => {
+    expect(searchCandies("Ma", 2)).toEqual(["Mars"]);
+  });
+  it("S, 10", () => {
+    expect(searchCandies("S", 10)).toEqual(["Skitties", "Skittles", "Starburst"]);
+  });
+  it("S, 10", () => {
+    expect(searchCandies("S", 4)).toEqual(["Skitties", "Skittles"]);
+  });
+  it("ma, 10", () => {
+    expect(searchCandies("ma", 10)).toEqual(["Mars", "Maltesers"]);
+  });
 });
