f45f0e9d2774b0dd8ebe29caf679d544b1537394
George Barrett <georgeaustinbarrett@gmail.com>
2023-05-24 13:52:10 +0100

    new test for adding multiple items and getting the total price. test in test file. running GREEN

--- week1_challenges/shopping_basket/shoppingBasket.test.js
+++ week1_challenges/shopping_basket/shoppingBasket.test.js
@@ -4,7 +4,7 @@ describe('ShoppingBasket', () => {
     it('has a total price of zero when intialized', () => {
         const basket = new ShoppingBasket();
         expect(basket.getTotalPrice()).toBe(0);
-    })
+    });
 
     it('has a new price when an item is added', () => {
         const basket = new ShoppingBasket();
@@ -16,5 +16,24 @@ describe('ShoppingBasket', () => {
 
         basket.addItem(mockCandy);
         expect(basket.getTotalPrice()).toBe(4.99);
-    })
+    });
+
+    it('has a new price when three items are added', () => {
+        const basket = new ShoppingBasket();
+
+        const mockMars = {
+            getName: () => 'Mars',
+            getPrice: () => 4.99
+        };
+
+        const mockSkittle = {
+            getName: () => 'Skittles',
+            getPrice: () => 3.99
+        };
+
+        basket.addItem(mockMars);
+        basket.addItem(mockSkittle);
+        basket.addItem(mockSkittle);
+        expect(basket.getTotalPrice()).toBe(12.97);
+    });
 });
